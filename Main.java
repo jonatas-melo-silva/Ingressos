224d614e2110b84bc0e9a3b5ebf16f71c0c4adf6
Jonatas Melo Silva <jonatas891024@gmail.com>
2023-02-13 21:48:55 +0000

    feat: adiciona testes

--- Main.java
+++ Main.java
@@ -1,9 +1,23 @@
+import main.java.com.camarote.Camarote;
 import main.java.com.ingresso.*;
 
 class Main {
   public static void main(String[] args) {
     // criar ingresso
     Ingresso ingresso = new Ingresso(100.0);
-    System.out.println(ingresso.getValor());
+    System.out.println("Valor do ingresso: " + ingresso.getValor());
+
+    // criar ingresso com desconto
+    Ingresso ingressoComDesconto = new IngressoComDesconto(100.0, 99.0);
+    System.out.println("Valor do ingresso com desconto: " + ingressoComDesconto.getValor());
+
+    // criar ingresso vip
+    Ingresso ingressoVip = new IngressoVip(100.0, 10.0);
+    System.out.println("Valor do ingresso vip: " + ingressoVip.getValor());
+
+    // criar um camarote
+    Camarote camarote = new Camarote(100.0, 10.0, 20);
+    System.out.println("Valor do camarote: " + camarote.getValor());
+    System.out.println("Mesa: " + camarote.getMesa());
   }
 }
